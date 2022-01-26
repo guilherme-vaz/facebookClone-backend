9662a0ac4e2084664e5a02eedae37c14dcf5d734
Guilherme Vaz <guilhermeolivaaz@gmail.com>
2022-01-26 17:02:57 -0300

    first commit

---
+++ app/Models/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export { default as User } from './User'
+export { default as UserKey } from './UserKey'
---
+++ start/routes/users.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.post('/users/register', 'Users/Register.store')
+Route.get('/users/register/:key', 'Users/Register.show')
+Route.put('/users/register', 'Users/Register.update')
